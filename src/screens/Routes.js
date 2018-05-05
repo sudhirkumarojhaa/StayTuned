2b977f5a88ad995429f9676e4c2dd012fdffc534
James Sudhir <jamessudhir@Jamess-Sudhir.local>
2018-05-05 14:12:28 +0530

    initial commit

---
+++ src/screens/Routes.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { StackNavigator } from 'react-navigation';
+import NewsScreen from "./NewsScreen";
+
+
+export default MyNewProject = StackNavigator({
+  NewsScreen: {
+    screen: NewsScreen
+  },
+ 
+});
+
+
